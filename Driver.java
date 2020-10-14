51dd790cbb69b638f923806f6de3d8d4b29d674b
Nur Ain Syafikah <ansyfkh@gmail.com>
2020-10-14 22:43:59 +0800

    first commit

---
+++ Driver.java
@@ -0,0 +1,31 @@
+public class Driver {
+	
+	public static void main(String [] arg) {
+		//Fish actually is a data type
+		//Fish nemo; == int x;
+		//nemo adalah object, Fish adalah kelas
+		Fish nemo = new Fish();  
+
+		nemo.Swim();
+		//nemo.color = "red"; // tukar value
+		nemo.SetColor("red"); //change here
+		nemo.Swim();
+
+		//nemo.Eat(); //superclass call subclass method
+
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharpteeth(130);
+		fiery.Eat();
+
+		//Fish dory = new Fish(); //another object
+		//dory.Swim();
+		//dory.color = "yellow";
+		//dory.SetColor("yellow"); //change here
+		//dory.Swim();
+
+		Aquirium fancyAquirium = new Aquirium();
+		fancyAquirium.fillFish();
+	}
+}
